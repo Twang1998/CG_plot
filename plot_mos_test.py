b08a17a6177215fc8c573e0f56d9da3d866c583c
Twang1998 <371515509@qq.com>
2021-12-28 21:03:10 +0800

    change distplot to hist and kde #20211228

--- plot_mos_test.py
+++ plot_mos_test.py
@@ -4,7 +4,8 @@ import cv2
 import os 
 import matplotlib.pyplot as plt
 import seaborn as sns
-plt.rc('font',family='Times New Roman',size=20)
+# plt.style.use('ggplot')
+plt.rc('font',family='Times New Roman',size=24)
 # plt.style.use(['science', 'no-latex'])
 df = pd.read_csv('MOS_test.csv')
 img_list = np.array(df['Image'])
@@ -36,20 +37,24 @@ for i,img in enumerate(img_list):
 #### seprate
 #### together
 # distplot 简版就是hist 加上一根density curve
+
 fig = plt.figure(figsize=(16,8))
-sns.set_palette("hls")
+plt.grid(ls='--',zorder=0) 
+# sns.set_palette("hls")
 # plt.rc("figure", figsize=(9, 5))
-sns.distplot(game_MOS,bins = 20,kde_kws={"color": "r", "lw":3, "label" : "Game" }, 
-             hist_kws={'edgecolor':'k',"histtype": "stepfilled", "color": "tomato" ,"range": [0,5]})
-sns.distplot(movie_MOS,bins = 20,kde_kws={"color": "b", "lw":3, "label" : "Movie" }, 
-             hist_kws={"histtype": "stepfilled", "color": "royalblue","range": [0,5] })
+sns.histplot(game_MOS,bins = 20,kde=False,stat="density",binrange=(0,5),color='tomato',zorder=10)
+sns.kdeplot(game_MOS,color='r',lw = 3,label='Game',zorder=30)
+
+sns.histplot(movie_MOS,bins = 20,kde=False,stat="density",binrange=(0,5),color='royalblue',zorder=20)
+sns.kdeplot(movie_MOS,color='b',lw = 3,label='Movie',zorder=40)
+
 # sns.distplot(MOS_list,kde_kws={"color": "seagreen", "lw":3, "label" : "Kernel Density Estimation" },hist=None)
 plt.xlabel('MOS')
 plt.ylabel('Probability density')
 plt.xlim(0,5)
 plt.title('Probability density of Game and Movie separately')
 plt.legend(fancybox=False,frameon = False)
-plt.grid() 
+
 # plt.savefig('MOS_game_movie_dist.pdf',dpi = 500,bbox_inches = 'tight',transparent = True)
 plt.show()
 
